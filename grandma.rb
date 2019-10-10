74cc3d0050c046b62d1ed1fdaa4c96e997236c6e
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-10 23:14:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
-def speak_to_grandma
-  if speak_to_grandma == "Hi Nana, how are you?"
-    puts "HUH?! SPEAK UP, SONNY"
-  elsif speak_to_grandma == "I LOVE YOU GRANDMA!".upcase
-    puts "I LOVE YOU TOO PUMKIN!"
-  else speak_to_grandma =="CAN YOU HEAR ME?".upcase
-    puts "NO, NOT SINCE 1938!"
-  end
+def speak_to_grandma(phrase)
+  if phrase == "Hi Nana, how are you?"
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "YOU CANT HEAR ME?"
+    puts "NO, NOT SINCE 1938"
+  else
+    phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 end
 
 
